f485f9096fe939ee19fed4e524bfc8d9e1a1df5f
SMMS28 <116540844+SMMS28@users.noreply.github.com>
2023-05-04 22:14:00 +0530

    Update sum_of_integers.java

--- sum_of_integers.java
+++ sum_of_integers.java
@@ -14,7 +14,6 @@ class sum_of_integers{
             System.out.println(n);
             sum = sum + n;
         }
-     
         System.out.println("sum of the integers is: " + sum);
         sc.close();
     }
